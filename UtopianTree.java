ac838a8b39efe8f0330c9203f179d1f1ae93920d
Preethika <112964321+preethika97@users.noreply.github.com>
2022-11-01 20:23:49 +0530

    Create UtopianTree.java

---
+++ UtopianTree.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+  public static int utopianTree(int n) {
+        int height=1;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==1)
+            height *= 2;
+         else
+           height += 1;
+        }
+        return height;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            int result = Result.utopianTree(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
