46ab6f8ea59202d5a51792df1964f501d29af910
Preethika <112964321+preethika97@users.noreply.github.com>
2022-12-01 20:40:47 +0530

    Create PrintTheChar.java

---
+++ 30.11.22/PrintTheChar.java
@@ -0,0 +1,44 @@
+/*Given two string of same length, print the characters which does not 
+match in the two. 
+ 
+  Example : 
+    Input    : String 1 = “abcdefeghijklm” 
+               String 2 = “abcduabihijkim” 
+    Output :   Result = “eufaebgili”*/ 
+
+package Task_28;
+
+import java.util.*;
+
+public class PrintTheChar {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the first String:");
+		String n = s.next();
+		System.out.println("enter the second string:");
+		String n1 = s.next();
+		String s1 = " ";
+		int len = 0, len1 = 0;
+		{
+			for (char ch : n.toCharArray()) {
+				len++;
+			}
+			for (char ch : n1.toCharArray()) {
+				len1++;
+			}
+			for (int i = 0; i < len; i++) {
+				for (int j = 0; j < len1; j++) {
+
+					if (n.charAt(i) != n1.charAt(j)) {
+
+						s1 += n.charAt(i);
+						s1 += n1.charAt(j);
+					}
+					i++;
+				}
+			}
+			System.out.println(s1);
+		}
+	}
+}
