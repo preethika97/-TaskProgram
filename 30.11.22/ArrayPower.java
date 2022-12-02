66054252013f63d9c19b84e9b7c38304f97895c0
Preethika <112964321+preethika97@users.noreply.github.com>
2022-12-02 13:31:52 +0530

    Update ArrayPower.java

--- 30.11.22/ArrayPower.java
+++ 30.11.22/ArrayPower.java
@@ -23,12 +23,12 @@ public class ArrayPower {
 	public static void main(String[] args) {
 
 		Scanner  sc= new Scanner(System.in);
-		System.out.println("Enter the Limit : "); 
+		System.out.print("Enter the Limit : "); 
 		int n = sc.nextInt();
 		int arr[] = new int[n];
-		System.out.println("Enter the power: ");
+		System.out.print("Enter the power: ");
 		int pow = sc.nextInt(), temp;  
-		System.out.println("Enter Elements : ");
+		System.out.println("Enter the Elements : ");
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 			int tempPow = pow; 
